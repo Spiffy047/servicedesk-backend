ca5dd45fee46b48d6f4c93bf8c0607f2fa98dd6f
Samwel Maina <abere.maina@gmail.com>
2025-10-27 15:58:57 +0300

    DESK-204: Add enhanced user routes with notification preferences

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,13 +1,19 @@
+"""
+Users Routes Module
+Enhanced user management with notification preferences
+"""
+
 from flask import Blueprint, request, jsonify
 from app import db
 from app.models.user import User
-from app.schemas.user_schema import user_schema, users_schema
+from app.services.notification_service import NotificationService
+from datetime import datetime
 
 users_bp = Blueprint('users', __name__)
 
 @users_bp.route('/', methods=['GET'])
 def get_users():
-    """Get all users"""
+    """Get all users with optional role filtering"""
     role = request.args.get('role')
     
     query = User.query
@@ -15,49 +21,25 @@ def get_users():
         query = query.filter(User.role == role)
     
     users = query.order_by(User.name).all()
-    return jsonify(users_schema.dump(users))
+    return jsonify([{
+        'id': user.id,
+        'name': user.name,
+        'email': user.email,
+        'role': user.role
+    } for user in users])
 
-@users_bp.route('/<user_id>', methods=['GET'])
-def get_user(user_id):
-    """Get a specific user"""
+@users_bp.route('/<user_id>/notifications/preferences', methods=['GET'])
+def get_notification_preferences(user_id):
+    """Get user notification preferences"""
     user = User.query.get_or_404(user_id)
-    return jsonify(user_schema.dump(user))
-
-@users_bp.route('/', methods=['POST'])
-def create_user():
-    """Create a new user"""
-    data = request.get_json()
     
-    user = User(
-        id=data['id'],
-        name=data['name'],
-        email=data['email'],
-        role=data['role']
-    )
+    preferences = {
+        'user_id': user_id,
+        'user_email': user.email,
+        'email_enabled': True,
+        'sms_enabled': False,
+        'push_enabled': True,
+        'frequency': 'immediate'
+    }
     
-    db.session.add(user)
-    db.session.commit()
-    
-    return jsonify(user_schema.dump(user)), 201
-
-@users_bp.route('/<user_id>', methods=['PUT'])
-def update_user(user_id):
-    """Update a user"""
-    user = User.query.get_or_404(user_id)
-    data = request.get_json()
-    
-    for field in ['name', 'email', 'role']:
-        if field in data:
-            setattr(user, field, data[field])
-    
-    db.session.commit()
-    return jsonify(user_schema.dump(user))
-
-@users_bp.route('/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    """Delete a user"""
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    
-    return '', 204
+    return jsonify(preferences)
